b48b60e4c85f456f3710a06e62541b5d3aeb1323
Alex <mitrofanova21@yandex.ru>
2024-06-07 12:57:56 +0300

    Добавьте файлы проекта.

---
+++ WCF_Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using client_server;
+using System.ServiceModel;
+namespace WCF_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/client_server/Service1/x");
+            using (ServiceHost host = new ServiceHost(typeof(Service1), baseAddress))
+            {
+                host.Open();
+                Console.WriteLine("Сервис запущен.");
+                Console.WriteLine("Нажмите Enter для остановки сервиса...");
+                Console.ReadLine();
+
+                host.Close();
+
+            }
+        }
+    }
+}
